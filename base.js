de1ddfe4e51f394d75c6aecbe037efd052af5bee
steveluscher <me+github@steveluscher.com>
2022-11-15 22:41:56 +0000

    Create a base config

---
+++ base.js
@@ -0,0 +1,9 @@
+module.exports = {
+  env: {
+    es2020: true,
+    "shared-node-browser": true,
+  },
+  parserOptions: {
+    sourceType: "module",
+  },
+};
--- react.js
+++ react.js
@@ -1,1 +1,4 @@
-module.exports = {};
+const base = require("./base.js");
+module.exports = {
+  ...base,
+};
