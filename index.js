791f1dfdaa63255999e04bc05180695fda3e9840
steveluscher <me+github@steveluscher.com>
2022-11-15 23:11:23 +0000

    Unsorted keys are now an error, because that's the kind of ship I sail

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const base = require("./base.js");
 module.exports = {
   ...base,
   extends: ["eslint:recommended"],
+  plugins: ["sort-keys-fix"],
+  rules: {
+    "sort-keys-fix/sort-keys-fix": "error",
+  },
 };
